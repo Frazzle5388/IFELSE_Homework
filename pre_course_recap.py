b987a75ebc56a1aaa824a28e1b6340c7a63f7b2e
Fraser Dillon <fraserdillon050388@gmail.com>
2021-11-16 06:35:11 +0000

    first commit

---
+++ pre_course_recap.py
@@ -0,0 +1,13 @@
+#SET team = INPUT "Who will win the world cup in Qatar 2022? 
+#IF team is "scotland"
+#    THEN PRINT "Of course, who else!?"
+#ELSE
+#    PRINT "Are you mad? It can only be Scotland for sure!"
+#END
+
+team = input("Who will win the worldcup in Qatar 2022? ")
+
+if team == "scotland":
+    print("Of course, who else!?")
+else:
+    print("Are you mad? It can only be Scotland for sure!")
